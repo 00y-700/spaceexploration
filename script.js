25f420f50796d1aeecb5f51a32a5c6410e881235
denis matijevic <denisone90@gmail.com>
2020-06-30 20:37:50 -0600

    change commit before pull

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-     // AJAX call
+// AJAX call
      $.ajax({
         url: "https://api.nasa.gov/planetary/apod?api_key=6oFKRPYlitRe8khoY4zxJVrqOqD8GjisXLnWf2PJ",
         method: "GET"
@@ -8,4 +7,14 @@ $(document).ready(function(){
         const header = document.querySelector("#apod");
         header.setAttribute("style", `background-image:  url(${response.url}); background-position: center; background-repeat: no-repeat; background-size: cover;`);
       });
-})
+var quotes = [
+  "Just Do It -NIKE",
+  "Space, the final forntier. -Star Trek",
+  "The Earth is the cradle of humanity, but mankind cannot stay in the cradle forever. -Konstantin Tsiolkovsky",
+  "I know the sky is not the limit because there are footprints on the Moon - and I made some of them -Buzz Alrdin",
+  "There is no sound in outer space",
+]
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById("quoteDisplay").innerHTML = quotes[randomNumber];
+}
