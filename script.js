cbcdfadd84bd14ac637076cf189cc3f24e9e1ec2
YakiniA <yakinilatha09@gmail.com>
2020-07-07 00:06:17 -0600

    Changed color-Started Homepage

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
 var spaceMission = $(".spaceMission");
+var mainBlock = $(".main-block");
 var title= $(".title");
 
+function homePage(){
+    $(document).ready(function () {
+        // event.preventDefault();
+        $(".mainBlock-Contents").empty();
+        var divMain = $("<div class='homePageContent'>")
+        var homePageContent = $("<p class='homePageTitle' style='font-size: 20px; font-weight: bold; padding:10px'>");
+        homePageContent.text("Top Stories");
+//         <div id="search">    
+//         <input type="text" style="margin-bottom:30px;" name="searchText">
+//         <button id="submitBtn" class="fa fa-search"></button>        
+//   </div>
+        divMain.append(homePageContent);
+       
+        // h3.append(span);
+        var div = $("<form>");
+      
+        var input = $("<input type='text'  id='search' name='Textsearch'>");
+        var button = $("<button id='submitButton' class='searchBox'> <i class='material-icons'>search</i>");
+        div.append(input).append(button);
+        homePageContent.append(div);
+        divMain.append(div);
+        mainBlock.append(divMain);
+    });
+}
 
-    $.ajax({
-        url: "https://api.nasa.gov/planetary/apod?api_key=6oFKRPYlitRe8khoY4zxJVrqOqD8GjisXLnWf2PJ",	        
-        method: "GET"	     
-      }).then(function(response) {	     
-        console.log(response)
-        $("#apod").attr("src", response.url);
-    });      
 
 // APOD Button Click
 $(document).ready(function () {
 
-    
    $('.sidenav').sidenav();
    $(".apodBtn").on("click",function (event) {
     event.preventDefault();
+    $(".homePageContent").empty();
     $(".mainBlock-Contents").empty();
+    
     $(".apod").text("About APOD");
-});
+  });
 
 // Local Hubble View Button Click
 $(".localHubbleViewBtn").on("click", function(event){
     event.preventDefault();
+    $(".homePageContent").empty();
     $(".mainBlock-Contents").empty();
+   
     $(".localHubbleView").text("Local Sky View");
 });
 
 // Near Earth Objects
 $(".newEarthObjectsBtn").on("click", function(event){
     event.preventDefault();
+    $(".homePageContent").empty();
     $(".mainBlock-Contents").empty();
+  
     $(".nearEarthObjects").text("Near Earth Objects");
     
 });
@@ -39,7 +62,9 @@ $(".newEarthObjectsBtn").on("click", function(event){
 // Mars Weather
 $(".marsWeatherBtn").on("click", function(event){
     event.preventDefault();
+    $(".homePageContent").empty();
     $(".mainBlock-Contents").empty();
+  
     $(".marsWeather").text("Mars Weather");
     
 });
@@ -49,20 +74,22 @@ $(".spaceMissionBtn").on("click",function(event){
     console.log("Got click");
    
    event.preventDefault();
+   $(".homePageContent").empty();
     $(".mainBlock-Contents").empty();
+   
     var h3 = $("<h3 style='font-size: 22px; font-weight: bold;background-color: rgb(196, 187, 140); padding:15px'>");
     h3.text("Space Mission");
     spaceMission.append(h3);
     var spaceMissionDetails = $("<p class = details>").text("NASA has successfully launched over 200 crewed flights. Two have ended in failure, STS-51-L (the Challenger disaster) in 1986, and STS-107 (the Columbia disaster) in 2003. (Apollo 1 in 1967 lost three crew members but never launched.)");
     
-    var table= $("<table><thead><tr><th>Program</th><th>Start Date</th><th>End Date</th><th>No. of launched crewed missions</th><th>Notes</th> </tr> </thead> ");
+    var table= $("<table class='responsive-table'><thead><tr><th>Program</th><th>Start Date</th><th>End Date</th><th>Launched missions</th><th>Notes</th> </tr> </thead> ");
     var tableContents1 = $("<tbody><tr><td>Mercury Program</td><td>1959</td><td>1963</td><td>6</td><td>First U.S. crewed program</td> </tr>");
     var tableContents2 = $("<tr><td>Gemini Program</td><td>1963</td><td>1966</td><td>10</td><td>Program used to practice space rendezvous and EVAs</td> </tr>");
     var tableContents3 = $("<tr><td>Apollo Program</td><td>1961</td><td>1972</td><td>11</td><td>Brought first human to the Moon</td> </tr>");
     var tableContents4 = $("<tr><td>Skylab</td><td>1973</td><td>1974</td><td>3</td><td>first American space station</td> </tr>");
     var tableContents5 = $("<tr><td>Space Shuttle</td><td>1981</td><td>2011</td><td>135</td><td>First missions in which a spacecraft was reused</td> </tr>");
     var tableContents6 = $("<tr><td>International Space Station</td><td>1998</td><td>Ongoing</td><td>54</td><td>Joint with Roscosmos, CSA, ESA, and JAXA</td> </tr>");
-    var tableContents7 = $("<tr><td>Artemis program</td><td>2017</td><td>Ongoing</td><td>0</td><td>Current program to bring humans to the Moon again</td> </tr> </tbody> </table>");
+    var tableContents7 = $("<tr><td>Artemis program</td><td>2017</td><td>Ongoing</td><td>0</td><td>Current program to bring humans to the Moon again</td> </tr>");
 
     spaceMission.append(spaceMissionDetails);
     table.append(tableContents1);
@@ -77,10 +104,11 @@ $(".spaceMissionBtn").on("click",function(event){
 
 // Space Information
 $(".spaceInfoBtn").on("click", function(event){
-    event,preventDefault();
-    // $(".main-block").empty();
-    // $(".mainBlock-Title").empty();
+    event.preventDefault();
+   
+    $(".homePageContent").empty();
     $(".mainBlock-Contents").empty(); 
+   
     $(".spaceInfo").text("Space Information");
 
 });
@@ -88,13 +116,10 @@ $(".spaceInfoBtn").on("click", function(event){
 
 $(document).on("click", 'ul a', function(event){
     event.preventDefault();
-    // $(".main-block").empty();
-   //  $(".mainBlock-Title").empty();
-   // $(".mainBlock-Contents").empty();
+   
    $('div a').removeClass('active');
    $(this).addClass('active');
 
-//    const mq1 = window.matchMedia( "(max-width: 600px)" );
    const mq = window.matchMedia( "(max-width: 600px)" );
 
    if(mq.matches){
@@ -109,3 +134,4 @@ $(document).on("click", 'ul a', function(event){
   
 });
 
+homePage();
